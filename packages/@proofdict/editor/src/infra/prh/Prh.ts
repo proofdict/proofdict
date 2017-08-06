706d7575a9a1fbb50d08354e30c01db883377f13
azu <azuciao@gmail.com>
2017-08-06 19:48:51 +0900

    fix(prh): fix handling of regexp-like string /pattern/i
    Add workaround prh issue
    https://github.com/prh/prh/issues/25

--- packages/@proofdict/editor/src/infra/prh/Prh.ts
+++ packages/@proofdict/editor/src/infra/prh/Prh.ts
@@ -3,7 +3,7 @@ import { Engine } from "prh";
 import { Dictionary } from "../../domain/Dictionary";
 import { DictionarySpec } from "../../domain/DictionarySpec";
 
-export function testPattern(dictionary: Dictionary, spec: DictionarySpec): DictionarySpec | Error{
+export function testPattern(dictionary: Dictionary, spec: DictionarySpec): DictionarySpec | Error {
     if (spec.actual.length === 0) {
         return spec;
     }
@@ -14,10 +14,12 @@ export function testPattern(dictionary: Dictionary, spec: DictionarySpec): Dicti
     try {
         const engine = new Engine({
             version: 1,
-            rules: [{
-                expected: dictionary.expect.value,
-                patterns: dictionary.patterns.getPatternValuesWithoutEmpty()
-            }]
+            rules: patterns.map(pattern => {
+                return {
+                    expected: dictionary.expect.value,
+                    pattern: pattern
+                }
+            })
         });
         const expected = engine.replaceByRule("/web", spec.actual);
         return spec.updateExpected(expected);
