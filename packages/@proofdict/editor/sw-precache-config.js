2186f1c6393bf77dc19925a3c97569c691d5c3ca
azu <azuciao@gmail.com>
2017-08-11 00:39:02 +0900

    perf(html): add dict cache

---
+++ packages/@proofdict/editor/sw-precache-config.js
@@ -0,0 +1,10 @@
+// MIT © 2017 azu
+"use strict";
+module.exports = {
+    staticFileGlobs: [
+        'build/index.html',
+        'build/static/**/*.{js,css}',
+        'build/dict/*.dat.gz'
+    ],
+    stripPrefix: 'build/'
+};
