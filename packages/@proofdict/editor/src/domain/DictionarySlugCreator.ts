81ec4e2d96cddc8c4db9f164b845a67912484be6
azu <azuciao@gmail.com>
2017-08-11 21:54:51 +0900

    feat(tool): add convert tool

--- packages/@proofdict/editor/src/domain/DictionarySlugCreator.ts
+++ packages/@proofdict/editor/src/domain/DictionarySlugCreator.ts
@@ -3,20 +3,15 @@ import { Dictionary } from "./Dictionary";
 
 const sanitizeFileName = require("sanitize-filename");
 
+const escapeName = (fileName: string) => {
+    return fileName.replace(/\s/g, "_").replace(/^\./, "_");
+};
+
 /**
  * Create fileName-safe slug string from dictionary
  * @param {Dictionary} dictionary
  * @returns {string}
  */
 export function createSlugFromDictionary(dictionary: Dictionary): string {
-    if (dictionary.wordClasses.hasWordClass) {
-        const wordClassNames = dictionary.wordClasses
-            .toList()
-            .map(wordClass => {
-                return wordClass.pos;
-            })
-            .join("-");
-        return sanitizeFileName(`${dictionary.expected.value}--${wordClassNames}`).replace(/\s/g, "_");
-    }
-    return sanitizeFileName(dictionary.expected.value).replace(/\s/g, "_");
+    return sanitizeFileName(escapeName(`${dictionary.expected.value}--${dictionary.id.toValue()}`));
 }
--- packages/@proofdict/editor/tools/prh-to-json.ts
+++ packages/@proofdict/editor/tools/prh-to-json.ts
@@ -1,6 +1,27 @@
 // MIT © 2017 azu
-import prh from "prh";
+import * as prh from "prh";
+import { prhRuleToDictionary } from "../src/infra/prh/PrhRuleToDictionary";
+import { createSlugFromDictionary } from "../src/domain/DictionarySlugCreator";
+import * as path from "path";
+import * as fs from "fs";
+import { DictionarySerializer } from "../src/domain/Dictionary";
+import { jsonFormatter } from "../src/infra/formatter/JSONFormatter";
 
-const inputFile = "/Users/azu/.ghq/github.com/azu/prh.yml/javascript.yml";
-const result = prh.fromYAMLFilePath(inputFile);
-result.rules
+/**
+ * Usage: node ./prh-to-json.js input.yml /output/dir/
+ *
+ * Convert prh to json per dictionary.
+ */
+if (!(process.argv[2] && process.argv[3])) {
+    throw new Error("node file.js process-file output-dir");
+}
+const outputDir = path.resolve(process.cwd(), process.argv[3]);
+console.log("Process: " + process.argv[2]);
+const result = prh.fromYAMLFilePath(process.argv[2]);
+const jsonPromises = result.rules.map(prhRuleToDictionary);
+Promise.all(jsonPromises).then(dictionaries => {
+    dictionaries.forEach(dictionary => {
+        const slug = createSlugFromDictionary(dictionary);
+        fs.writeFileSync(path.join(outputDir, slug + ".json"), jsonFormatter(DictionarySerializer.toJSON(dictionary)), "utf-8");
+    });
+});
