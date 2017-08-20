686a86c09168bdb61f22a8a6717ee1c6944e4e93
azu <azuciao@gmail.com>
2017-08-20 19:09:30 +0900

    Change deploy to proofdict.github.io

---
+++ tools/push-to-github-io.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -eu
+
+declare currentDir=$(cd $(dirname $0);pwd)
+declare parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
+declare repositoryUrl="git@github.com:proofdict/proofdict.github.io.git"
+declare toBranch="master"
+declare commitMessage="Update website [skip ci]"
+declare commands="npm run build"
+declare distDir="${parentDir}/public"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  echo "This is Pull Request. Not deploy"
+  exit 0;
+fi
+# commit and push
+# commit_and_push_changes "message" "branch"
+commit_and_push_changes() {
+    # Commit and push changes upstream, and
+    # overwrite the content from the specified branch
+
+    git config --global user.email "$GH_USER_EMAIL" \
+        && git config --global user.name "$GH_USER_NAME" \
+        && git init \
+        && git add -A \
+        && git commit --message "$2" \
+        && (
+
+            # If the distribution branch is `master`,
+            # there is no need to switch as that is the default
+
+            if [ "$1" != "master" ]; then
+                git checkout --quiet -b "$1"
+            fi
+
+        ) \
+        && git push --quiet --force "${repositoryUrl}" "$1"
+
+}
+
+execute() {
+    eval ${1}
+}
+# remove_files_in_dir "dir"
+remove_files_in_dir() {
+    find "$1" -type f -delete
+}
+tmpDir=$(mktemp -d 2>/dev/null||mktemp -d -t tmp)
+echo "Clone content"
+git clone ${repositoryUrl} "${tmpDir}/proofdict.github.io"
+# delete all files
+echo "Remove files"
+remove_files_in_dir "${tmpDir}/proofdict.github.io"
+# execute command
+echo "Update content"
+execute "$commands"
+echo "Copy files"
+cp -Rf "${distDir}/"* "${tmpDir}/proofdict.github.io/"
+echo "Commit and push"
+cd "${tmpDir}/proofdict.github.io/"
+commit_and_push_changes  "${toBranch}" "${commitMessage}"
