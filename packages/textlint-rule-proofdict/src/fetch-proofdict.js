fc58364a86d29b2235d95a567da36bb03059509c
azu <azuciao@gmail.com>
2018-03-18 13:29:41 +0900

    fix(rule): support timeout error

--- packages/textlint-rule-proofdict/src/fetch-proofdict.js
+++ packages/textlint-rule-proofdict/src/fetch-proofdict.js
@@ -1,6 +1,32 @@
 // MIT © 2018 azu
 "use strict";
 const fetch = require("fetch-ponyfill")().fetch;
+
+function delayPromise(ms) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, ms);
+    });
+}
+
+function timeoutPromise(promise, ms) {
+    const timeout = delayPromise(ms).then(function() {
+        const error = new Error('Operation timed out after ' + ms + ' ms');
+        error.name = "TimeoutError";
+        throw error;
+    });
+    return Promise.race([promise, timeout]);
+}
+
 export function fetchProofdict({ URL }) {
-    return fetch(URL).then(res => res.json());
+    return timeoutPromise(fetch(URL), 5000).then(res => {
+        if (!res.ok) {
+            throw Error(res.statusText);
+        }
+        return res.json();
+    }).catch(error => {
+        if (error.name === "TimeoutError") {
+            return;
+        }
+        return Promise.reject(error);
+    });
 }
