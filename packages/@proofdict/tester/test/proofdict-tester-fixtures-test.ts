4296f9e72bf4a393c5ee091e215a7adf3878ae12
azu <azuciao@gmail.com>
2018-11-08 10:16:33 +0900

    chore(deps): upgrade deps

--- packages/@proofdict/tester/test/proofdict-tester-fixtures-test.ts
+++ packages/@proofdict/tester/test/proofdict-tester-fixtures-test.ts
@@ -5,15 +5,21 @@ import * as assert from "assert";
 describe("ProofdictTester fixtures", () => {
     const proofdict: ProofdictRule[] = require("./fixtures/proofdict.json");
     const tester = new ProofdictTester({ dictionary: proofdict });
-    proofdict.filter(dict => dict.specs.length > 0).forEach(dict => {
-        describe(`${dict.expected}`, () => {
-            dict.specs.forEach(spec => {
-                it(`${spec.from} => ${spec.to}`, () => {
-                    return tester.replace(spec.from).then(result => {
-                        assert.strictEqual(result, spec.to);
+    proofdict
+        .filter(dict => dict.specs && dict.specs.length > 0)
+        .forEach(dict => {
+            const proofdictRuleSpecs = dict.specs;
+            if (!proofdictRuleSpecs) {
+                return;
+            }
+            describe(`${dict.expected}`, () => {
+                proofdictRuleSpecs.forEach(spec => {
+                    it(`${spec.from} => ${spec.to}`, () => {
+                        return tester.replace(spec.from).then(result => {
+                            assert.strictEqual(result, spec.to);
+                        });
                     });
                 });
             });
         });
-    });
 });
