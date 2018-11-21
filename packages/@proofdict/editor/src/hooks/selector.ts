b121142012f4170c6b5ac63c72dd5a2fd5f0a91f
azu <azuciao@gmail.com>
2018-11-21 23:35:43 +0900

    add note

--- packages/@proofdict/editor/src/hooks/selector.ts
+++ packages/@proofdict/editor/src/hooks/selector.ts
@@ -4,6 +4,11 @@ import { shallowEqual } from "shallow-equal-object";
 /**
  * Create selector with shallow equality
  * @example
+ * ```
  * createShallowEqualSelector(selector, result => result.value);
+ * ```
+ * Note:
+ * selector should return memorize-able value
+ * combiner should return state instance
  */
 export const createShallowEqualSelector = createSelectorCreator(defaultMemoize, (a: any, b: any) => shallowEqual(a, b));
