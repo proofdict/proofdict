89b847d69df39fc532d1a9cefa26065fa9b89653
azu <azu@users.noreply.github.com>
2020-08-09 12:41:13 +0900

    fix(textlint-rule-proofdict): support browser field (#49)
    Pass
    
     npx can-bundle-it ./use.js --verbose --no-fs

---
+++ packages/@proofdict/textlint-rule-proofdict/src/fetch-dictionary/node.ts
@@ -0,0 +1,24 @@
+import * as globby from "globby";
+import yaml from "js-yaml";
+import { ProofdictRule } from "@proofdict/types";
+import { RuleOption } from "../RuleOptions";
+import { Proofdict } from "@proofdict/tester";
+import { MODE } from "../mode";
+
+export default (options: RuleOption, mode: MODE): Proofdict | undefined => {
+    if (mode !== MODE.LOCAL) {
+        return;
+    }
+    if (!options.dictGlob) {
+        return;
+    }
+    try {
+        const files = globby.sync(options.dictGlob);
+        return files.map((filePath) => {
+            return yaml.safeLoad(filePath) as ProofdictRule;
+        });
+    } catch (error) {
+        console.error(error);
+    }
+    return;
+};
